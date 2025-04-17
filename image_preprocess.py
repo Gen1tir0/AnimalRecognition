50da87a7d7ac30178121118afc5e4ceaa362dcd2
Gen1tir0 <160604274+Gen1tir0@users.noreply.github.com>
2025-04-17 17:48:37 +0300

    Update image_preprocess.py

--- image_preprocess.py
+++ image_preprocess.py
@@ -6,7 +6,8 @@ from PIL import Image
 
 def image_preprocess(byte_image):
     image = np.asarray(Image.open(BytesIO(byte_image)))
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     resized = cv2.resize(image, (64, 64))
-    expanded = np.reshape(resized, (1, 64, 64, 3))
+    expanded = np.reshape(resized, (1, 64, 64, 1))
     print(expanded.shape)
     return expanded
